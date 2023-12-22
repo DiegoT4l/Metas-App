19c83dcf0d8404db716d29c2a9c47bc6b6dbbbd7
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2023-12-22 14:11:11 -0600

    Add tailwindcss as a devDependency and update App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Footer from './components/Shared/Footer';
+import Header from './components/Shared/Header';
+import Principal from './components/Shared/Principal';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Principal />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Shared/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <small>Desarrollado y diseñado por DiegoT4l. &copy;2023. Todos los derechos reservados.</small>
+        </footer>
+    );
+}
+
+export default Footer;
